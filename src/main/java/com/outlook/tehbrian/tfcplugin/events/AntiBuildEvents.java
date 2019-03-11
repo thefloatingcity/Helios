4cc05b96a7764c0b918f019d649b9b45057c1e08
T3hBrian <tehbrian@outlook.com>
2019-03-11 11:40:54 -0500

    Added Listener.

--- src/main/java/com/outlook/tehbrian/tfcplugin/events/AntiBuildEvents.java
+++ src/main/java/com/outlook/tehbrian/tfcplugin/events/AntiBuildEvents.java
@@ -5,13 +5,14 @@ import com.outlook.tehbrian.tfcplugin.Misc;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.event.hanging.HangingPlaceEvent;
 import org.bukkit.event.player.*;
 
-public class AntiBuildEvents {
+public class AntiBuildEvents implements Listener {
 
     private final Main main;
 
