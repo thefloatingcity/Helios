0ffd904fcd0fc2bfa5528ec105388359d3648275
T3hBrian <tehbrian@outlook.com>
2019-04-10 00:44:03 -0400

    Testing out voting.

--- src/main/java/com/outlook/tehbrian/tfcplugin/events/MiscEvents.java
+++ src/main/java/com/outlook/tehbrian/tfcplugin/events/MiscEvents.java
@@ -4,6 +4,8 @@ import com.outlook.tehbrian.tfcplugin.Flight;
 import com.outlook.tehbrian.tfcplugin.Main;
 import com.outlook.tehbrian.tfcplugin.Misc;
 import com.outlook.tehbrian.tfcplugin.Piano;
+import com.vexsoftware.votifier.model.Vote;
+import com.vexsoftware.votifier.model.VotifierEvent;
 import org.bukkit.*;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
@@ -21,6 +23,12 @@ public class MiscEvents implements Listener {
 
     public MiscEvents(Main main) {
         this.main = main;
+    }
+
+    @EventHandler
+    public void onVotifierEvent(VotifierEvent event) {
+        Vote vote = event.getVote();
+        Bukkit.broadcastMessage(Misc.formatConfig("msg_voted", vote.getServiceName(), vote.getUsername()));
     }
 
     @EventHandler
