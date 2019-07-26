86e01fb732893ca282f1a60f01499476991f0a9c
T3hBrian <tehbrian@outlook.com>
2019-07-26 01:54:12 -0500

    Removed excessive nesting from a lot of code, fixed rotating glazed terracotta not working, made slab breaking code a tad bit less redundant, changed the ordering of some checks.

--- src/main/java/com/outlook/tehbrian/tfcplugin/events/BuildingEvents.java
+++ src/main/java/com/outlook/tehbrian/tfcplugin/events/BuildingEvents.java
@@ -74,143 +74,123 @@ public class BuildingEvents implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onSignClick(PlayerInteractEvent event) {
-        if (event.getPlayer().getGameMode() == GameMode.CREATIVE) {
-            if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                if (event.getHand() == EquipmentSlot.HAND) {
-                    BlockState blockState = event.getClickedBlock().getState();
-                    if (blockState instanceof Sign) {
-                        if (!event.getPlayer().isSneaking()) {
-                            if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.SIGN) {
-                                Bukkit.getScheduler().runTask(main, () -> {
-                                    Sign sign = (Sign) blockState;
-                                    event.getPlayer().openSign(sign);
-                                });
-                                event.setCancelled(true);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (event.getHand() != EquipmentSlot.HAND) return;
+        if (event.getPlayer().getGameMode() != GameMode.CREATIVE) return;
+        if (event.getPlayer().isSneaking()) return;
+
+        BlockState blockState = event.getClickedBlock().getState();
+        if (!(blockState instanceof Sign)) return;
+        if (event.getPlayer().getInventory().getItemInMainHand().getType() != Material.SIGN) return;
+
+        Bukkit.getScheduler().runTask(main, () -> {
+            Sign sign = (Sign) blockState;
+            event.getPlayer().openSign(sign);
+        });
+        event.setCancelled(true);
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onIronTrapDoorInteract(PlayerInteractEvent event) {
-        if (event.getPlayer().getGameMode() == GameMode.CREATIVE) {
-            if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                if (event.getHand() == EquipmentSlot.HAND) {
-                    if (event.getClickedBlock().getType() == Material.IRON_TRAPDOOR) {
-                        if (!event.getPlayer().isSneaking()) {
-                            if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.AIR) {
-                                Bukkit.getScheduler().runTask(main, () -> {
-                                    Block block = event.getClickedBlock();
-                                    byte data = block.getData();
-                                    byte newData = 0;
-                                    if (data >= 0 && data < 4) {
-                                        newData = (byte) (data + 4);
-                                    } else if (data >= 4 && data < 8) {
-                                        newData = (byte) (data - 4);
-                                    } else if (data >= 8 && data < 12) {
-                                        newData = (byte) (data + 4);
-                                    } else if (data >= 12 && data < 16) {
-                                        newData = (byte) (data - 4);
-                                    }
-                                    block.setData(newData, true);
-                                });
-                                event.setCancelled(true);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (event.getHand() != EquipmentSlot.HAND) return;
+        if (event.getPlayer().getGameMode() != GameMode.CREATIVE) return;
+        if (event.getPlayer().isSneaking()) return;
+        if (event.getClickedBlock().getType() != Material.IRON_TRAPDOOR) return;
+        if (event.getPlayer().getInventory().getItemInMainHand().getType() != Material.AIR) return;
 
+        Bukkit.getScheduler().runTask(main, () -> {
+            Block block = event.getClickedBlock();
+            byte data = block.getData();
+            byte newData = 0;
+            if (data >= 0 && data < 4) {
+                newData = (byte) (data + 4);
+            } else if (data >= 4 && data < 8) {
+                newData = (byte) (data - 4);
+            } else if (data >= 8 && data < 12) {
+                newData = (byte) (data + 4);
+            } else if (data >= 12 && data < 16) {
+                newData = (byte) (data - 4);
+            }
+            block.setData(newData, true);
+        });
+        event.setCancelled(true);
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onGlazedTerracottaInteract(PlayerInteractEvent event) {
-        if (event.getPlayer().getGameMode() == GameMode.CREATIVE) {
-            if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                if (event.getHand() == EquipmentSlot.HAND) {
-                    if (event.getClickedBlock().getType().name().contains("GLAZED")) {
-                        if (event.getPlayer().isSneaking()) {
-                            if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.AIR) {
-                                Bukkit.getScheduler().runTask(main, () -> {
-                                    Block block = event.getClickedBlock();
-                                    byte data = block.getData();
-                                    byte newData = (byte) (data + 1);
-                                    if (data < 0 || data >= 4) {
-                                        data = 0;
-                                    }
-                                    block.setData(data, true);
-                                });
-                                event.setCancelled(true);
-                            }
-                        }
-                    }
-                }
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (event.getHand() != EquipmentSlot.HAND) return;
+        if (event.getPlayer().getGameMode() != GameMode.CREATIVE) return;
+        if (!event.getPlayer().isSneaking()) return;
+        if (!event.getClickedBlock().getType().name().contains("GLAZED")) return;
+        if (event.getPlayer().getInventory().getItemInMainHand().getType() != Material.AIR) return;
+
+        Bukkit.getScheduler().runTask(main, () -> {
+            Block block = event.getClickedBlock();
+            byte data = block.getData();
+            byte newData = (byte) (data + 1);
+            if (data < 0 || data >= 4) {
+                newData = 0;
             }
-        }
+            block.setData(newData, true);
+        });
+        event.setCancelled(true);
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onSlabBreak(PlayerInteractEvent event) {
-        if (event.getPlayer().getGameMode() == GameMode.CREATIVE) {
-            if (event.getAction() == Action.LEFT_CLICK_BLOCK) {
-                Material material = event.getPlayer().getInventory().getItemInMainHand().getType();
-                if (material != Material.STEP && material != Material.WOOD_STEP && material != Material.STONE_SLAB2 && material != Material.PURPUR_SLAB) {
-                    return;
+        if (event.getAction() != Action.LEFT_CLICK_BLOCK) return;
+        if (event.getPlayer().getGameMode() != GameMode.CREATIVE) return;
+
+        Material material = event.getPlayer().getInventory().getItemInMainHand().getType();
+        if (material != Material.STEP && material != Material.WOOD_STEP && material != Material.STONE_SLAB2 && material != Material.PURPUR_SLAB) return;
+
+        Block block = event.getClickedBlock();
+        byte data = block.getData();
+
+        if (data <= 7) {
+            if (block.getType() == Material.DOUBLE_STEP) {
+                event.setCancelled(true);
+                if (MiscUtils.isTop(event.getPlayer(), block)) {
+                    block.setType(Material.STEP);
+                    block.setData(data);
+                } else {
+                    block.setType(Material.STEP);
+                    block.setData((byte) (data + 8));
                 }
+            }
 
-                Block block = event.getClickedBlock();
-                byte data;
-
-                if (block.getType() == Material.DOUBLE_STEP && block.getData() <= 7) {
-                    event.setCancelled(true);
-                    data = block.getData();
-                    if (MiscUtils.isTop(event.getPlayer(), block)) {
-                        block.setType(Material.STEP);
-                        block.setData(data);
-                    } else {
-                        block.setType(Material.STEP);
-                        block.setData((byte) (data + 8));
-                    }
+            if (block.getType() == Material.WOOD_DOUBLE_STEP) {
+                event.setCancelled(true);
+                if (MiscUtils.isTop(event.getPlayer(), block)) {
+                    block.setType(Material.WOOD_STEP);
+                    block.setData(data);
+                } else {
+                    block.setType(Material.WOOD_STEP);
+                    block.setData((byte) (data + 8));
                 }
+            }
 
-                if (block.getType() == Material.WOOD_DOUBLE_STEP && block.getData() <= 7) {
-                    event.setCancelled(true);
-                    data = block.getData();
-                    if (MiscUtils.isTop(event.getPlayer(), block)) {
-                        block.setType(Material.WOOD_STEP);
-                        block.setData(data);
-                    } else {
-                        block.setType(Material.WOOD_STEP);
-                        block.setData((byte) (data + 8));
-                    }
+            if (block.getType() == Material.DOUBLE_STONE_SLAB2) {
+                event.setCancelled(true);
+                if (MiscUtils.isTop(event.getPlayer(), block)) {
+                    block.setType(Material.STONE_SLAB2);
+                    block.setData(data);
+                } else {
+                    block.setType(Material.STONE_SLAB2);
+                    block.setData((byte) (data + 8));
                 }
+            }
 
-                if (block.getType() == Material.DOUBLE_STONE_SLAB2 && block.getData() <= 7) {
-                    event.setCancelled(true);
-                    data = block.getData();
-                    if (MiscUtils.isTop(event.getPlayer(), block)) {
-                        block.setType(Material.STONE_SLAB2);
-                        block.setData(data);
-                    } else {
-                        block.setType(Material.STONE_SLAB2);
-                        block.setData((byte) (data + 8));
-                    }
-                }
-
-                if (block.getType() == Material.PURPUR_DOUBLE_SLAB && block.getData() <= 7) {
-                    event.setCancelled(true);
-                    data = block.getData();
-                    if (MiscUtils.isTop(event.getPlayer(), block)) {
-                        block.setType(Material.PURPUR_SLAB);
-                        block.setData(data);
-                    } else {
-                        block.setType(Material.PURPUR_SLAB);
-                        block.setData((byte) (data + 8));
-                    }
+            if (block.getType() == Material.PURPUR_DOUBLE_SLAB) {
+                event.setCancelled(true);
+                if (MiscUtils.isTop(event.getPlayer(), block)) {
+                    block.setType(Material.PURPUR_SLAB);
+                    block.setData(data);
+                } else {
+                    block.setType(Material.PURPUR_SLAB);
+                    block.setData((byte) (data + 8));
                 }
             }
         }
