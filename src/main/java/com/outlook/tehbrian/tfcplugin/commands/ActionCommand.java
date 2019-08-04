de4f7f5d03d47df97496973d78b142e3c3d87d22
TehBrian <tehbrian@outlook.com>
2019-08-04 01:06:25 -0400

    Made action msg config keys more explicit.

--- src/main/java/com/outlook/tehbrian/tfcplugin/commands/ActionCommand.java
+++ src/main/java/com/outlook/tehbrian/tfcplugin/commands/ActionCommand.java
@@ -42,7 +42,7 @@ public class ActionCommand extends BaseCommand {
         targetPlayer.getWorld().playSound(targetPlayer.getLocation(), Sound.ENTITY_FIREWORK_LAUNCH, SoundCategory.MASTER, 5, 0.75F);
 
         if (target == null) {
-            Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.launch").formats(player.getDisplayName()).build());
+            Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.launch_self").formats(player.getDisplayName()).build());
         } else {
             Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.launch_other").formats(player.getDisplayName(), targetPlayer.getDisplayName()).build());
         }
@@ -59,7 +59,7 @@ public class ActionCommand extends BaseCommand {
         targetPlayer.getWorld().playSound(targetPlayer.getEyeLocation(), Sound.ENTITY_FIREWORK_LAUNCH, SoundCategory.MASTER, 5, 0.75F);
 
         if (target == null) {
-            Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.boost").formats(player.getDisplayName()).build());
+            Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.boost_self").formats(player.getDisplayName()).build());
         } else {
             Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.boost_other").formats(player.getDisplayName(), targetPlayer.getDisplayName()).build());
         }
@@ -75,7 +75,7 @@ public class ActionCommand extends BaseCommand {
         targetPlayer.getWorld().strikeLightning(targetPlayer.getLocation());
 
         if (target == null) {
-            Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.zap").formats(player.getDisplayName()).build());
+            Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.zap_self").formats(player.getDisplayName()).build());
         } else {
 
             Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.zap_other").formats(player.getDisplayName(), targetPlayer.getDisplayName()).build());
@@ -102,7 +102,7 @@ public class ActionCommand extends BaseCommand {
         targetPlayer.setVelocity(randomVector);
 
         if (target == null) {
-            Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.poke").formats(player.getDisplayName()).build());
+            Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.poke_self").formats(player.getDisplayName()).build());
         } else {
             Bukkit.broadcastMessage(new MsgBuilder().def("msg.action.poke_other").formats(player.getDisplayName(), targetPlayer.getDisplayName()).build());
         }
