548b0c93f9dec274251b0ad2c48f410c0457b9a9
TehBrian <tehbrian@outlook.com>
2019-11-09 16:10:56 -0500

    Use String#format for ConfigUtils#fancifyTime

--- src/main/java/xyz/tehbrian/tfcplugin/util/MiscUtils.java
+++ src/main/java/xyz/tehbrian/tfcplugin/util/MiscUtils.java
@@ -14,15 +14,15 @@ public class MiscUtils {
 
     public static String fancifyTime(long milliseconds) {
         if (milliseconds >= 86400000) {
-            return new MsgBuilder().msgKey("msg.fancify_time_format").formats(Math.floor((milliseconds / 86400000d) * 100) / 100, "days").build();
+            return new MsgBuilder().msgKey("msg.fancify_time_format").formats(milliseconds / 86400000d, "days").build();
         } else if (milliseconds >= 3600000) {
-            return new MsgBuilder().msgKey("msg.fancify_time_format").formats(Math.floor((milliseconds / 3600000d) * 100) / 100, "hours").build();
+            return new MsgBuilder().msgKey("msg.fancify_time_format").formats(milliseconds / 3600000d, "hours").build();
         } else if (milliseconds >= 60000) {
-            return new MsgBuilder().msgKey("msg.fancify_time_format").formats(Math.floor((milliseconds / 60000d) * 100) / 100, "minutes").build();
+            return new MsgBuilder().msgKey("msg.fancify_time_format").formats(milliseconds / 60000d, "minutes").build();
         } else if (milliseconds >= 1000) {
-            return new MsgBuilder().msgKey("msg.fancify_time_format").formats(Math.floor((milliseconds / 1000d) * 100) / 100, "seconds").build();
+            return new MsgBuilder().msgKey("msg.fancify_time_format").formats(milliseconds / 1000d, "seconds").build();
         } else {
-            return new MsgBuilder().msgKey("msg.fancify_time_format").formats(Math.floor((milliseconds) * 100) / 100, "milliseconds").build();
+            return new MsgBuilder().msgKey("msg.fancify_time_format").formats(milliseconds, "milliseconds").build();
         }
     }
 }
