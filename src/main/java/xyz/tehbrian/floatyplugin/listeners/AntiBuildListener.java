c4d8e86f3475d56e1c9aec30e8ecd90bcd1c6c15
TehBrian <tehbrian@protonmail.com>
2021-10-03 18:31:09 -0400

    lower event priority in AntiBuildListener

--- src/main/java/xyz/tehbrian/floatyplugin/listeners/AntiBuildListener.java
+++ src/main/java/xyz/tehbrian/floatyplugin/listeners/AntiBuildListener.java
@@ -34,68 +34,68 @@ public final class AntiBuildListener implements Listener {
         this.langConfig = langConfig;
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onBlockPlace(final BlockPlaceEvent event) {
         this.onAntiBuild(event, event.getPlayer());
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onBlockBreak(final BlockBreakEvent event) {
         this.onAntiBuild(event, event.getPlayer());
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onHangingPlace(final HangingPlaceEvent event) {
         if (event.getPlayer() != null) {
             this.onAntiBuild(event, event.getPlayer());
         }
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onHangingBreak(final HangingBreakByEntityEvent event) {
         if (event.getRemover() instanceof Player player) {
             this.onAntiBuild(event, player);
         }
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onBucketFill(final PlayerBucketFillEvent event) {
         this.onAntiBuild(event);
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onBucketEmpty(final PlayerBucketEmptyEvent event) {
         this.onAntiBuild(event);
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onItemPickup(final PlayerAttemptPickupItemEvent event) {
         this.onAntiBuild(event, true);
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onItemDrop(final PlayerDropItemEvent event) {
         this.onAntiBuild(event, true);
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onInteract(final PlayerInteractEvent event) {
         this.onAntiBuild(event);
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onEntityDamageByEntity(final EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player player) {
             this.onAntiBuild(event, player);
         }
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onArmorStandManipulate(final PlayerArmorStandManipulateEvent event) {
         this.onAntiBuild(event);
     }
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.LOW)
     public void onInteractEntity(final PlayerInteractEntityEvent event) {
         this.onAntiBuild(event);
     }
