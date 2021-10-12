2def1613a7c1be3ed0c8523c5d1a7c682f21ebf0
TehBrian <tehbrian@protonmail.com>
2021-10-12 19:47:10 -0400

    remove useless comments in ServerPingListener

--- src/main/java/xyz/tehbrian/floatyplugin/listeners/ServerPingListener.java
+++ src/main/java/xyz/tehbrian/floatyplugin/listeners/ServerPingListener.java
@@ -20,19 +20,6 @@ public final class ServerPingListener implements Listener {
         this.langConfig = langConfig;
     }
 
-//    no-permission: §cI'm sorry, but you don't have permission for that command.
-//    connection-throttle: §c»§6»§e»§f Connection throttled. Please wait a moment before reconnecting. §e«§6«§c«
-//    authentication-servers-down: §4»§c»§6»§f Mojang's authentication servers seem to be down. Try joining again in a bit! §6«§c«§4«
-//    flying-player: §f»§7»§8»§f You can't fly in The Floating City! Perhaps it was a glitch, or maybe lag? §8«§7«§f«
-//    flying-vehicle: §f»§7»§8»§f You can't fly in The Floating City! Perhaps it was a glitch, or maybe lag? §8«§7«§f«
-//    whitelist: §f»§7»§8»§f You aren't whitelisted on this server! §8«§7«§f«
-//    unknown-command: §fUnrecognized command. Try §e/help§f for a list of commands.
-//    server-full: §4»§c»§6»§f The Floating City is currently full! Try joining again in a bit! §6«§c«§4«
-//    outdated-client: §1»§9»§3»§f The Floating City is running a newer version! Please use {0} §3«§9«§1«
-//    outdated-server: §1»§9»§3»§f The Floating City is running an older version! Please use {0} §3«§9«§1«
-//    restart: §9»§3»§b»§f The Floating City is restarting. Come back in a moment! §b«§3«§9«
-//    shutdown-message: §f»§7»§8»§f The Floating City has been closed. See ya soon! §8«§7«§f«
-
     @EventHandler
     public void onServerPing(final PaperServerListPingEvent e) {
         e.motd(this.langConfig.c(NodePath.path("server_motd")));
