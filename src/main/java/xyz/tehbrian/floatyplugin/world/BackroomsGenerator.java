bf59050c29d8e6230d5eb9b58b75b069c95f605c
TehBrian <tehbrian@protonmail.com>
2022-08-13 22:39:18 -0400

    perf: use #setRegion in BackroomsGenerator

--- src/main/java/xyz/tehbrian/floatyplugin/world/BackroomsGenerator.java
+++ src/main/java/xyz/tehbrian/floatyplugin/world/BackroomsGenerator.java
@@ -3,6 +3,7 @@ package xyz.tehbrian.floatyplugin.world;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Light;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.generator.WorldInfo;
@@ -13,7 +14,6 @@ import java.util.Random;
 
 public final class BackroomsGenerator extends ChunkGenerator {
 
-  public static final Material AIR = Material.AIR;
   public static final Material LIGHT = Material.LIGHT;
   public static final Material WALL = Material.SMOOTH_SANDSTONE;
   public static final Material SPACE = Material.STONE_BRICKS;
@@ -30,22 +30,46 @@ public final class BackroomsGenerator extends ChunkGenerator {
   ) {
     int result = 0;
     final var posX = chunkData.getBlockData(x + 1, y, z).getMaterial();
-    if (posX == AIR || posX == LIGHT) {
+    if (posX != WALL) {
       result++;
     }
     final var negX = chunkData.getBlockData(x - 1, y, z).getMaterial();
-    if (negX == AIR || negX == LIGHT) {
+    if (negX != WALL) {
       result++;
     }
     final var posZ = chunkData.getBlockData(x, y, z + 1).getMaterial();
-    if (posZ == AIR || posZ == LIGHT) {
+    if (posZ != WALL) {
       result++;
     }
     final var negZ = chunkData.getBlockData(x, y, z - 1).getMaterial();
-    if (negZ == AIR || negZ == LIGHT) {
+    if (negZ != WALL) {
       result++;
     }
     return result;
+  }
+
+  private static void setColumn(
+      final ChunkData chunkData,
+      final int chunkX, final int middleY, final int chunkZ, final Material material
+  ) {
+    // wondering why the positions look so funny?
+    // remember: ChunkData#setRegion max positions are EXCLUSIVE.
+    chunkData.setRegion(
+        chunkX, middleY - 1, chunkZ,
+        chunkX + 1, middleY + 2, chunkZ + 1,
+        material
+    );
+  }
+
+  private static void setColumn(
+      final ChunkData chunkData,
+      final int chunkX, final int middleY, final int chunkZ, final BlockData blockData
+  ) {
+    chunkData.setRegion(
+        chunkX, middleY - 1, chunkZ,
+        chunkX + 1, middleY + 2, chunkZ + 1,
+        blockData
+    );
   }
 
   @Override
@@ -61,6 +85,13 @@ public final class BackroomsGenerator extends ChunkGenerator {
       final int chunkZ,
       final @NotNull ChunkData chunkData
   ) {
+    // entire world other than the main area is solid.
+    chunkData.setRegion(
+        0, chunkData.getMinHeight(), 0,
+        16, chunkData.getMaxHeight(), 16,
+        SPACE
+    );
+
     // dimmed light data for use in light blocks.
     final Light lightData = ((Light) LIGHT.createBlockData());
     lightData.setLevel(10);
@@ -70,11 +101,6 @@ public final class BackroomsGenerator extends ChunkGenerator {
 
     for (int x = 0; x < 16; x++) {
       for (int z = 0; z < 16; z++) {
-        // entire world other than the main area is solid.
-        for (int y = chunkData.getMinHeight(); y < chunkData.getMaxHeight(); y++) {
-          chunkData.setBlock(x, y, z, SPACE);
-        }
-
         // open sides won't ever be less than 2 because of our line-by-line
         // approach to looping over positions.
         final int openSides = openSides(x, middleY, z, chunkData);
@@ -83,9 +109,7 @@ public final class BackroomsGenerator extends ChunkGenerator {
 
         if (generateWall) {
           // wall column.
-          chunkData.setBlock(x, middleY + 1, z, WALL);
-          chunkData.setBlock(x, middleY, z, WALL);
-          chunkData.setBlock(x, middleY - 1, z, WALL);
+          setColumn(chunkData, x, middleY, z, WALL);
 
           // add variety to the wall.
           if (random.nextFloat() < 0.01F) {
@@ -99,12 +123,8 @@ public final class BackroomsGenerator extends ChunkGenerator {
           }
         } else {
           // empty column of light blocks.
-          chunkData.setBlock(x, middleY + 1, z, LIGHT);
-          chunkData.setBlock(x, middleY + 1, z, lightData);
-          chunkData.setBlock(x, middleY, z, LIGHT);
-          chunkData.setBlock(x, middleY, z, lightData);
-          chunkData.setBlock(x, middleY - 1, z, LIGHT);
-          chunkData.setBlock(x, middleY - 1, z, lightData);
+          setColumn(chunkData, x, middleY, z, LIGHT);
+          setColumn(chunkData, x, middleY, z, lightData);
         }
       }
     }
