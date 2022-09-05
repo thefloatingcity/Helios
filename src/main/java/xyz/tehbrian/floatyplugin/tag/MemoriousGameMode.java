873f5e84af69090857e977f23c9d6081756a993d
TehBrian <tehbrian@proton.me>
2022-09-05 13:24:30 -0400

    make MemoriousGameMode final

--- src/main/java/xyz/tehbrian/floatyplugin/tag/MemoriousGameMode.java
+++ src/main/java/xyz/tehbrian/floatyplugin/tag/MemoriousGameMode.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public class MemoriousGameMode {
+public final class MemoriousGameMode {
 
   private final Map<Player, GameMode> previousGameModes = new HashMap<>();
 
