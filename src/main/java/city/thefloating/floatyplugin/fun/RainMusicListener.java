52ad5c4169de16b4ed56217c6ac22a9f04be2578
TehBrian <tehbrian@proton.me>
2023-10-20 00:53:11 -0400

    clean up sound handling in RainMusicListener

--- src/main/java/city/thefloating/floatyplugin/fun/RainMusicListener.java
+++ src/main/java/city/thefloating/floatyplugin/fun/RainMusicListener.java
@@ -1,17 +1,12 @@
 package city.thefloating.floatyplugin.fun;
 
-import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.audience.ForwardingAudience;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.sound.SoundStop;
-import org.bukkit.SoundCategory;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
-import java.util.List;
 import java.util.Random;
 
 public final class RainMusicListener implements Listener {
@@ -24,15 +19,11 @@ public final class RainMusicListener implements Listener {
 
   @EventHandler
   public void onWeatherChange(final WeatherChangeEvent event) {
-    final List<Player> playersList = event.getWorld().getPlayers();
-    final ForwardingAudience playersAudience = Audience.audience(playersList);
     if (event.toWeatherState() && RANDOM.nextFloat() <= 0.2) { // 20% chance.
-      for (final Player player : playersList) {
-        player.stopSound(SoundCategory.MUSIC);
-      }
-      playersAudience.playSound(MUSIC);
+      event.getWorld().stopSound(SoundStop.source(Sound.Source.MUSIC));
+      event.getWorld().playSound(MUSIC);
     } else {
-      playersAudience.stopSound(MUSIC_STOP);
+      event.getWorld().stopSound(MUSIC_STOP);
     }
   }
 
