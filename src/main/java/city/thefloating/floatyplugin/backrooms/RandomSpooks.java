12123e49fcd421722766c131ebb4d2a5d06eb0bc
TehBrian <tehbrian@proton.me>
2023-10-21 16:39:40 -0400

    backrooms: adjust spook randomness

--- src/main/java/city/thefloating/floatyplugin/backrooms/RandomSpooks.java
+++ src/main/java/city/thefloating/floatyplugin/backrooms/RandomSpooks.java
@@ -2,15 +2,19 @@ package city.thefloating.floatyplugin.backrooms;
 
 import city.thefloating.floatyplugin.FloatyPlugin;
 import city.thefloating.floatyplugin.Ticks;
+import city.thefloating.floatyplugin.nextbot.Nate;
+import city.thefloating.floatyplugin.nextbot.Nextbot;
 import city.thefloating.floatyplugin.realm.Realm;
 import city.thefloating.floatyplugin.realm.WorldService;
 import com.google.inject.Inject;
 import net.kyori.adventure.sound.Sound;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.util.Vector;
 
 import java.time.Duration;
 import java.util.Random;
@@ -22,14 +26,17 @@ public final class RandomSpooks {
 
   private final FloatyPlugin plugin;
   private final WorldService worldService;
+  private final Nate nate;
 
   @Inject
   public RandomSpooks(
       final FloatyPlugin plugin,
-      final WorldService worldService
+      final WorldService worldService,
+      final Nate nate
   ) {
     this.plugin = plugin;
     this.worldService = worldService;
+    this.nate = nate;
   }
 
   public void start() {
@@ -39,17 +46,19 @@ public final class RandomSpooks {
     // slowed down cave noises.
     scheduler.scheduleSyncRepeatingTask(this.plugin, () -> {
       for (final Player player : this.worldService.getWorld(Realm.BACKROOMS).getPlayers()) {
-        if (RANDOM.nextFloat() < 0.2F) {
-          player.playSound(Sound.sound(org.bukkit.Sound.AMBIENT_CAVE, Sound.Source.MASTER, 10F, 0.6F));
+        if (RANDOM.nextFloat() > 0.2F) {
+          continue;
         }
+
+        player.playSound(Sound.sound(org.bukkit.Sound.AMBIENT_CAVE, Sound.Source.MASTER, 10F, 0.6F));
       }
-    }, 1, Ticks.in(Duration.ofSeconds(29)));
+    }, 1, Ticks.in(Duration.ofSeconds(29)) - 3);
 
     // high-pitched noise and jitter camera.
     scheduler.scheduleSyncRepeatingTask(this.plugin, () -> {
       for (final Player player : this.worldService.getWorld(Realm.BACKROOMS).getPlayers()) {
-        if (RANDOM.nextFloat() > 0.1F) {
-          continue; // random chance.
+        if (RANDOM.nextFloat() > 0.05F) {
+          continue;
         }
 
         if (player.getLocation().getBlock().getLightLevel() >= 4) {
@@ -62,17 +71,18 @@ public final class RandomSpooks {
             100,
             RANDOM.nextFloat(0.7F, 1.9F)
         ));
+
         final var loc = player.getLocation();
         loc.setYaw(loc.getYaw() + RANDOM.nextInt(-10, 10));
         loc.setPitch(loc.getPitch() + RANDOM.nextInt(-10, 10));
         player.teleport(loc);
       }
-    }, 1, Ticks.in(Duration.ofSeconds(6)));
+    }, 1, Ticks.in(Duration.ofSeconds(6)) - 4);
 
-    // temporary teleport to black area and slower noise.
+    // temporary teleport to black area and low-pitched noise.
     scheduler.scheduleSyncRepeatingTask(this.plugin, () -> {
       for (final Player player : this.worldService.getWorld(Realm.BACKROOMS).getPlayers()) {
-        if (RANDOM.nextFloat() > 0.05F) {
+        if (RANDOM.nextFloat() > 0.1F) {
           continue;
         }
 
@@ -85,16 +95,15 @@ public final class RandomSpooks {
 
         final var previous = player.getLocation();
         final var up = previous.clone().add(0, 50, 0);
-
         player.teleport(up);
         scheduler.runTaskLater(this.plugin, () -> player.teleport(previous), 7);
       }
     }, 1, Ticks.in(Duration.ofSeconds(57)) - 7);
 
-    // deep boom sound plus blindness.
+    // deep boom noise plus blindness.
     scheduler.scheduleSyncRepeatingTask(this.plugin, () -> {
       for (final Player player : this.worldService.getWorld(Realm.BACKROOMS).getPlayers()) {
-        if (RANDOM.nextFloat() > 0.13F) {
+        if (RANDOM.nextFloat() > 0.15F) {
           continue;
         }
 
@@ -106,7 +115,7 @@ public final class RandomSpooks {
         ));
         player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 30, 1, true, true, false));
       }
-    }, 1, Ticks.in(Duration.ofMinutes(2)) - 17);
+    }, 1, Ticks.in(Duration.ofSeconds(123)) - 17);
   }
 
 }
