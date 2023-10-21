776bd353ce1cd83a71ee6ab616bce7507d2cba7a
TehBrian <tehbrian@proton.me>
2023-10-21 16:28:07 -0400

    backrooms: clean up RandomSpooks

--- src/main/java/city/thefloating/floatyplugin/backrooms/RandomSpooks.java
+++ src/main/java/city/thefloating/floatyplugin/backrooms/RandomSpooks.java
@@ -7,26 +7,18 @@ import city.thefloating.floatyplugin.realm.WorldService;
 import com.google.inject.Inject;
 import net.kyori.adventure.sound.Sound;
 import org.bukkit.Server;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitScheduler;
 
 import java.time.Duration;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
-import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 public final class RandomSpooks {
 
   private static final Random RANDOM = ThreadLocalRandom.current();
-
-  private static final Duration JITTER_COOLDOWN = Duration.ofSeconds(269);
-  private final Map<UUID, Instant> lastJitter = new HashMap<>();
 
   private final FloatyPlugin plugin;
   private final WorldService worldService;
@@ -46,8 +38,7 @@ public final class RandomSpooks {
 
     // slowed down cave noises.
     scheduler.scheduleSyncRepeatingTask(this.plugin, () -> {
-      final World backrooms = this.worldService.getWorld(Realm.BACKROOMS);
-      for (final Player player : backrooms.getPlayers()) {
+      for (final Player player : this.worldService.getWorld(Realm.BACKROOMS).getPlayers()) {
         if (RANDOM.nextFloat() < 0.2F) {
           player.playSound(Sound.sound(org.bukkit.Sound.AMBIENT_CAVE, Sound.Source.MASTER, 10F, 0.6F));
         }
@@ -56,22 +47,13 @@ public final class RandomSpooks {
 
     // high-pitched noise and jitter camera.
     scheduler.scheduleSyncRepeatingTask(this.plugin, () -> {
-      final World backrooms = this.worldService.getWorld(Realm.BACKROOMS);
-      final var now = Instant.now();
-      for (final Player player : backrooms.getPlayers()) {
+      for (final Player player : this.worldService.getWorld(Realm.BACKROOMS).getPlayers()) {
         if (RANDOM.nextFloat() > 0.1F) {
           continue; // random chance.
         }
 
         if (player.getLocation().getBlock().getLightLevel() >= 4) {
           continue; // player must be in dark area.
-        }
-
-        if (this.lastJitter.containsKey(player.getUniqueId())
-            && Duration
-            .between(now, this.lastJitter.get(player.getUniqueId()))
-            .compareTo(JITTER_COOLDOWN) < 0) {
-          continue; // not too often.
         }
 
         player.playSound(Sound.sound(
@@ -84,15 +66,12 @@ public final class RandomSpooks {
         loc.setYaw(loc.getYaw() + RANDOM.nextInt(-10, 10));
         loc.setPitch(loc.getPitch() + RANDOM.nextInt(-10, 10));
         player.teleport(loc);
-
-        this.lastJitter.put(player.getUniqueId(), Instant.now());
       }
     }, 1, Ticks.in(Duration.ofSeconds(6)));
 
     // temporary teleport to black area and slower noise.
-    scheduler.runTaskTimer(this.plugin, () -> {
-      final World backrooms = this.worldService.getWorld(Realm.BACKROOMS);
-      for (final Player player : backrooms.getPlayers()) {
+    scheduler.scheduleSyncRepeatingTask(this.plugin, () -> {
+      for (final Player player : this.worldService.getWorld(Realm.BACKROOMS).getPlayers()) {
         if (RANDOM.nextFloat() > 0.05F) {
           continue;
         }
@@ -113,9 +92,8 @@ public final class RandomSpooks {
     }, 1, Ticks.in(Duration.ofSeconds(57)) - 7);
 
     // deep boom sound plus blindness.
-    scheduler.runTaskTimer(this.plugin, () -> {
-      final World backrooms = this.worldService.getWorld(Realm.BACKROOMS);
-      for (final Player player : backrooms.getPlayers()) {
+    scheduler.scheduleSyncRepeatingTask(this.plugin, () -> {
+      for (final Player player : this.worldService.getWorld(Realm.BACKROOMS).getPlayers()) {
         if (RANDOM.nextFloat() > 0.13F) {
           continue;
         }
